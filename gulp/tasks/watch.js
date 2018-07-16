41c1ff58121a38f00f82e2e9cf75cb4ba7010eb1
nxde <nxde@ctd.com>
2018-07-16 09:34:15 +0700

    Completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp=require("gulp"),
+    watch=require("gulp-watch"),    
+    browserSync=require("browser-sync").create();
+
+gulp.task("watch",function(){
+    
+    browserSync.init({
+        browser: "chrome",
+        notify: false,//not show the notifying message when the site is changing its display
+        server:{
+            baseDir:"app"
+        }
+    });
+    
+    watch("./app/index.html",function(){
+       browserSync.reload();
+    });
+    
+    watch("./app/assets/styles/**/*.css",function(){
+        gulp.start("cssInject");
+    });
+});
+
+gulp.task("cssInject", ["styles"],function(){
+   return gulp.src("./app/assets/styles/style.css")
+        .pipe(browserSync.stream());
+});
